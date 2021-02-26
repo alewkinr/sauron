0fe09920a6b0fbfbf4581c20658d149ea1931755
Haspel <>
2021-02-26 20:59:13 +0300

    Init commit

---
+++ aas21/src/main/java/ru/sber/aas21/Aas21Application.java
@@ -0,0 +1,13 @@
+package ru.sber.aas21;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Aas21Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Aas21Application.class, args);
+    }
+
+}
---
+++ aas21/src/test/java/ru/sber/aas21/Aas21ApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.sber.aas21;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Aas21ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
