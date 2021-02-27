b66249d9396c343a08718216d2f705fe42600623
Ramil Aleshkin <rraleshk@mts.ru>
2021-02-27 12:35:03 +0300

    🐛 Убрал компонент SearchInputItem, убрал мок редьюсера

--- app/store/index.tsx
+++ app/store/index.tsx
@@ -1,13 +1,10 @@
 import { applyMiddleware, combineReducers, createStore } from "redux";
 import thunk from "redux-thunk";
 
-import { MainReducer } from "../reducers/MainReducer";
-
 import { ServiceReducer } from "./Service/reducer";
 
 const middleware = applyMiddleware(thunk);
 const rootReducers = combineReducers({
-  main: MainReducer,
   service: ServiceReducer,
 });
 
