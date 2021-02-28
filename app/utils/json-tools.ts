72c98405b39fcc2c9242c3b55de1f238c72eb5ca
Ramil Aleshkin <rraleshk@mts.ru>
2021-02-28 11:15:46 +0300

    🐛 Испаравил визуал графиков, теперь они смотрятся сильно лучше

--- app/utils/json-tools.ts
+++ app/utils/json-tools.ts
@@ -1,8 +1,10 @@
 import { ChartData } from "../components/Chart/Chart";
 import { ResponseMetrics } from "../api/cpu-usage/contracts";
 
+import { dateShortener } from "./time-tools";
+
 export function toLineChartDataSet(input: ResponseMetrics): ChartData {
-  const labels = input.datapoints.map((md) => `${md.ts}`);
+  const labels = input.datapoints.map((md) => dateShortener(md.ts));
   const data = input.datapoints.map((md) => md.min);
 
   return {
